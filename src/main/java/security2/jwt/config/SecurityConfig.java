661009a28e7b665b479c3d8e0de136af2fc7ac9c
rogitun <rogitun@gmail.com>
2022-04-22 23:51:24 +0900

    Initial commit

---
+++ src/main/java/security2/jwt/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package security2.jwt.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //세션 안쓰겠다.
+                .and()
+                .addFilter(corsFilter()) //모든 요청은 이 필터를 거친다. 시큐리티 필터에 등록 인증
+                .formLogin().disable() //폼 로그인 안한다.
+                .httpBasic().disable()
+                .authorizeRequests()
+                .antMatchers("/api/v1/user/**")
+                .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/manager/**")
+                .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/admin/**")
+                .access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll();
+    }
+
+    @Bean
+    public CorsFilter corsFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); //내 서버가 응답할때 json을 자바스크립트에서 처리할 수 있게 설정하는것
+        config.addAllowedOrigin("*"); //모든 ip에 응답 허용
+        config.addAllowedHeader("*"); //모든 헤더에 응답 허용
+        config.addAllowedMethod("*"); //모든 post,get,put,delete 요청을 허용하겠다.
+
+        source.registerCorsConfiguration("/api/**",config);
+        return new CorsFilter(source);
+    }
+}
